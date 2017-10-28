24b837dee2ffc41ba3897dfe3d3d64b69f762976
aspittel <ali07cat07@gmail.com>
2017-10-28 16:08:04 -0400

    standard

--- script.js
+++ script.js
@@ -28,111 +28,102 @@ boxes.forEach(box => box.addEventListener('click', boxSelect))
 
 document.getElementById('reset').addEventListener('click', resetGame)
 
-function togglePlayer() {
-    turn = turn === 0 ? 1 : 0
-    turnDiv.innerHTML = `Player ${currentChip()} it's your turn!`
+function togglePlayer () {
+  turn = turn === 0 ? 1 : 0
+  turnDiv.innerHTML = `Player ${currentChip()} it's your turn!`
 }
 
-
-function currentChip() {
-    return turn === 0 ? 'O' : 'X'
+function currentChip () {
+  return turn === 0 ? 'O' : 'X'
 }
 
-
-function incrementDiagonals(x, y) {
-    if (y === x) diagTally[0][turn]++
-        if ((y === 2 && x === 0) ||
+function incrementDiagonals (x, y) {
+  if (y === x) diagTally[0][turn]++
+  if ((y === 2 && x === 0) ||
             (y === 1 && x === 1) ||
             (y === 0 && x === 2)) diagTally[1][turn]++
 }
 
+function incrementChecks (x, y) {
+  numMoves++
 
-function incrementChecks(x, y) {
-    numMoves++
+  rowTally[x][turn]++
+  colTally[y][turn]++
 
-    rowTally[x][turn]++
-        colTally[y][turn]++
-
-        incrementDiagonals(x, y)
+  incrementDiagonals(x, y)
 }
 
-
-function checkWin(x, y) {
-    return (rowTally[x][turn] === 3 ||
+function checkWin (x, y) {
+  return (rowTally[x][turn] === 3 ||
         colTally[y][turn] === 3 ||
         diagTally[0][turn] === 3 ||
         diagTally[1][turn] === 3)
 }
 
-
-function resetBox(box) {
-    box.innerHTML = ''
-    box.setAttribute('data-clicked', 'false')
+function resetBox (box) {
+  box.innerHTML = ''
+  box.setAttribute('data-clicked', 'false')
 }
 
-
-function resetGame() {
-    rowTally = [
+function resetGame () {
+  rowTally = [
         [0, 0],
         [0, 0],
         [0, 0]
-    ]
-    colTally = [
+  ]
+  colTally = [
         [0, 0],
         [0, 0],
         [0, 0]
-    ]
-    diagTally = [
+  ]
+  diagTally = [
         [0, 0],
         [0, 0]
-    ]
-    clicked = [
+  ]
+  clicked = [
         [false, false, false],
         [false, false, false],
         [false, false, false]
-    ]
-    numMoves = 0
-    boxes.forEach(box => resetBox(box))
+  ]
+  numMoves = 0
+  boxes.forEach(box => resetBox(box))
 }
 
 function disableAll () {
-    clicked = [
+  clicked = [
         [true, true, true],
         [true, true, true],
         [true, true, true]
-    ]
+  ]
 }
 
-function checkReset(x, y) {
-    const winner = checkWin(x, y)
-    if (winner || numMoves >= 9) {
-        const message = winner ? `Congrats ${currentChip()} you win!` : `No winner!`
-        turnDiv.innerHTML = message
-        disableAll()
-        return true
-    }
+function checkReset (x, y) {
+  const winner = checkWin(x, y)
+  if (winner || numMoves >= 9) {
+    const message = winner ? `Congrats ${currentChip()} you win!` : `No winner!`
+    turnDiv.innerHTML = message
+    disableAll()
+    return true
+  }
 }
 
-
-function editHTML(el) {
-    el.innerHTML = currentChip()
+function editHTML (el) {
+  el.innerHTML = currentChip()
 }
 
+function updateGame (x, y) {
+  clicked[x][y] = true
+  incrementChecks(x, y)
+  let win = checkReset(x, y)
 
-function updateGame(x, y) {
-    clicked[x][y] = true
-    incrementChecks(x, y)
-    let win = checkReset(x, y)
-
-    if (!win) togglePlayer()
+  if (!win) togglePlayer()
 }
 
+function boxSelect (e) {
+  const x = parseInt(this.dataset.x)
+  const y = parseInt(this.dataset.y)
+  if (clicked[x][y]) return
 
-function boxSelect(e) {
-    const x = parseInt(this.dataset.x)
-    const y = parseInt(this.dataset.y)
-    if (clicked[x][y]) return
-
-    editHTML(this)
-    updateGame(x, y)
+  editHTML(this)
+  updateGame(x, y)
 }
